591038819307a1111e4c78467c1a77cf02a71cf9
BigRamy#0318 <john.kolahdouzan@gmail.com>
2021-04-30 03:58:28 -0400

    init commit

--- ibmcloud-depolyment.sh
+++ ibmcloud-depolyment.sh
@@ -12,17 +12,27 @@
 #kubectl get ingress -n csi-dev  --watch
 #kubectl get svc -n csi-dev
 
-
-
-ibmcloud login -a cloud.ibm.com -apikey YxaF7mgbfnGmQJSckZiMxlrVwPj0Ssr4MIGLEAV20Tiu --no-region
+ibmcloud login -a cloud.ibm.com -apikey YxaF7mgbfnGmQJSckZiMxlrVwPj0Ssr4MIGLEAV20Tiu -r us-east -g Default
+ibmcloud ks cluster config --cluster c229pavw0ht6snnn198g
+ibmcloud plugin install container-service
+ibmcloud plugin install observe-service
+ibmcloud plugin list
+kubectl config current-context
 
 ibmcloud cr build --no-cache --quiet --tag us.icr.io/bb-web/bb-web:lts --build-arg JAR_FILE=./target/*.jar .
+sudo docker build -t johnkola/bb-web:lts --build-arg JAR_FILE=./target/*.jar .
+sudo docker run johnkola/bb-web:lts
+sudo docker push johnkola/bb-web:lts
+kubectl get nodes
 
-helm install bb-web ./eih-base-helm --namespace csi-dev --create-namespace
+#helm install bb-web ./eih-base-helm --namespace csi-dev --create-namespace
+helm delete web-app --namespace csi-dev
+helm install  web-app ./web-app --namespace csi-dev --create-namespace
+helm template web-app ./web-app --namespace csi-dev --create-namespace
+helm upgrade  web-app ./web-app --namespace csi-dev --create-namespace
 
+docker push us.icr.io/bb-web/bb-web:lts
 
-
-
-
+docker login -u iamapikey -p YxaF7mgbfnGmQJSckZiMxlrVwPj0Ssr4MIGLEAV20Tiu https://private.us.icr.io
 
 
